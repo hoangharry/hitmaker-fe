bcb4815f04c489eb35e87fadb97b27179a9a7a54
PhuongNguyen <thuphuong.nguyen.cntt@gmail.com>
2021-08-04 17:10:41 +0700

    [r] refactor toolbar to function component

--- src/components/Buttons/index.js
+++ src/components/Buttons/index.js
@@ -16,69 +16,69 @@ import semiquaverLogo from 'src/pictures/semiquaver.png'
 
 import './index.css'
 
-const ToolbarButton = ({handleClick, imgSrc, className=''}) => {
+const ToolbarButton = ({onClick, imgSrc, className=''}) => {
   return (
-    <Button onClick={handleClick} className={`toolbar-button ${className}`}>
+    <Button onClick={onClick} className={`toolbar-button ${className}`}>
       <img className="button-icon" src={imgSrc} alt="img"/>
     </Button>
   )
 }
 
-const AddButton = (handleClick) => {
+const AddButton = ({handleClick}) => {
   return <ToolbarButton onClick={handleClick} imgSrc={addIcon}/>
 }
 
-const SaveButton = (handleClick) => {
+const SaveButton = ({handleClick}) => {
   return <ToolbarButton onClick={handleClick} imgSrc={saveIcon}/>
 }
 
-const SemibreveBtn = (handleClick) => {
+const SemibreveBtn = ({handleClick}) => {
   const onClick = () => { handleClick('semibreve') }
   return <ToolbarButton onClick={onClick} imgSrc={semibreveLogo}/>
 }
 
-const MinimBtn = (handleClick) => {
+const MinimBtn = ({handleClick}) => {
   const onClick = () => { handleClick('minim') }
   return <ToolbarButton onClick={onClick} imgSrc={minimLogo}/>
 }
 
-const CrotchetBtn = (handleClick) => {
+const CrotchetBtn = ({handleClick}) => {
   const onClick = () => { handleClick('crotchet') }
   return <ToolbarButton onClick={onClick} imgSrc={crochetLogo}/>
 }
 
-const QuaverBtn = (handleClick) => {
+const QuaverBtn = ({handleClick}) => {
   const onClick = () => { handleClick('quaver') }
   return <ToolbarButton onClick={onClick} imgSrc={quaverLogo}/>
 }
 
-const SemiQuaverBtn = (handleClick) => {
+const SemiQuaverBtn = ({handleClick}) => {
   const onClick = () => { handleClick('semiquaver') }
   return <ToolbarButton onClick={onClick} imgSrc={semiquaverLogo}/>
 }
 
-const DemisemiQuaverBtn = (handleClick) => {
+const DemisemiQuaverBtn = ({handleClick}) => {
   const onClick = () => { handleClick('demisemiquaver') }
   return <ToolbarButton onClick={onClick} imgSrc={demisemiquaverLogo}/>
 }
 
-const PlayButton = (handleClick) => {
+const PlayButton = ({handleClick}) => {
   return <ToolbarButton onClick={handleClick} imgSrc={playIcon}/>
 }
 
-const PauseButton = (handleClick) => {
+const PauseButton = ({handleClick}) => {
   return <ToolbarButton onClick={handleClick} imgSrc={pauseIcon}/>
 }
 
-const DownloadButton = (handleClick) => {
+const DownloadButton = ({handleClick}) => {
   return <ToolbarButton onClick={handleClick} imgSrc={downloadIcon}/>
 }
 
-const GenerateButton = (handleClick) => {
+const GenerateButton = ({handleClick}) => {
   return <ToolbarButton onClick={handleClick} imgSrc={generateIcon}/>
 }
 
-const BackspaceButton = (handleClick) => {
+const BackspaceButton = ({handleClick}) => {
   return <ToolbarButton onClick={handleClick} imgSrc={backspaceIcon}/>
 }
 
