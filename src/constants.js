c4dee776449d04cd1a602ec901a7a9c7b570ff89
PhuongNguyen <thuphuong.nguyen.cntt@gmail.com>
2021-08-05 11:07:50 +0700

    [c] setup env

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const  API_URL = process.env.API_URL
+
+export const DEFAULT_CLEF = ['treble', 'bass']
+
+export const KEY_SIGNATURES = ['C', 'G', 'D', 'A', 'E', 'B', 'F#', 'C#', 'F', 'B-', 'E-', 'A-',
+  'D-', 'G-', 'C-']
+
+export const NOTE_RANGES = ['C/2', 'D/2', 'E/2', 'F/2', 'G/2', 'A/2', 'B/2', 'C/3', 'D/3', 'E/3', 'F/3', 'G/3', 'A/3', 'B/3',
+  'C/4', 'D/4', 'E/4', 'F/4', 'G/4', 'A/4', 'B/4', 'C/5', 'D/5', 'E/5', 'F/5', 'G/5', 'A/5', 'B/5', 'C/6']
+
+export const FIRST_NOTE = 'C/2'
