4775d12b7137e207cbae25eb40530d916add08e1
PhuongNguyen <thuphuong.nguyen.cntt@gmail.com>
2021-08-07 23:30:58 +0700

    [c] Add logo

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,7 @@ import { useHistory } from 'react-router-dom'
 import { Navbar, Container, Nav } from 'react-bootstrap'
 import { useContext } from 'react'
 import { SongInfoContext } from '../context/SongInfoContext'
+import logo from 'src/pictures/logo.png'
 import { logout } from '../service/auth.service'
 
 export function About() {
@@ -38,21 +39,23 @@ export function TopNavbar() {
 
   if (nameUsr !== undefined && nameUsr !== null) {
     navlinks = <Nav>
-      <Nav.Link onClick ={() => history.push('/user')}>{nameUsr}</Nav.Link>
-      <Nav.Link onClick ={() => onLogout()}>Log out</Nav.Link>
-      <Nav.Link onClick ={() => history.push('/')}>About</Nav.Link>
+      <Nav.Link style={{color: '#e3cf1e'}} onClick ={() => history.push('/user')}>{nameUsr}</Nav.Link>
+      <Nav.Link style={{color: '#e3cf1e'}} className="custom-nav-link" onClick ={() => onLogout()}>Log out</Nav.Link>
+      <Nav.Link style={{color: '#e3cf1e'}} className="custom-nav-link" onClick ={() => history.push('/')}>About</Nav.Link>
     </Nav>
   } else {
     navlinks = <Nav>
-      <Nav.Link onClick={() => history.push('/login')}>Log in</Nav.Link>
-      <Nav.Link onClick={() => history.push('/signup')}>Sign up</Nav.Link>
-      <Nav.Link onClick={() => history.push('/')}>About</Nav.Link>
+      <Nav.Link style={{color: '#e3cf1e'}} className="custom-nav-link" onClick={() => history.push('/login')}>Log in</Nav.Link>
+      <Nav.Link style={{color: '#e3cf1e'}} className="custom-nav-link" onClick={() => history.push('/signup')}>Sign up</Nav.Link>
+      <Nav.Link style={{color: '#e3cf1e'}} className="custom-nav-link" onClick={() => history.push('/')}>About</Nav.Link>
     </Nav>
   }
   return (
     <Navbar collapseOnSelect sticky="top" expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand>HIT MAKER</Navbar.Brand>
+        <Navbar.Brand>
+          <img src={logo} alt="HIT MAKER" className="hit-maker-logo"/>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
           {navlinks}
